9f7cc87b5cdc78a5dd0114dcf342fc5e46163788
Sothearith Tith <captainteemo32@gmail.com>
2025-04-21 14:30:59 +1000

    updated requirements.txt

--- app.py
+++ app.py
@@ -8,17 +8,16 @@ load_dotenv()
 
 app = Flask(__name__)
 
-# # Google sheet authentication
-# SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
-# creds = Credentials.from_service_account_file("svi-products-7b8def7059e2.json", scopes=SCOPES)
-# client = gspread.authorize(creds)
-
 # Google Sheets authentication
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 
 # Load credentials from environment variable
-service_account_info = json.loads(os.environ["GOOGLE_CREDENTIALS"])
-creds = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+try:
+    service_account_info = json.loads(os.environ["GOOGLE_CREDENTIALS"])
+    creds = Credentials.from_service_account_info(service_account_info, scopes=SCOPES)
+except KeyError:
+    raise RuntimeError("GOOGLE_CREDENTIALS environment variable not set")
+
 
 # Authorize the client
 client = gspread.authorize(creds)
@@ -36,6 +35,11 @@ def convert_link(link):
         except IndexError:
             return link # return original link if format is incorrect
     return link # return original link if not Google Drive link
+
+def sheet_to_dict(worksheet):
+    data = worksheet.get_all_values()
+    headers = data[0]
+    return [dict(zip(headers, row)) for row in data[1:]]
 
 # Route to display all categories
 @app.route('/category')
@@ -106,15 +110,13 @@ def show_product(category_name, product_id):
         product = next((p for p in products if p["ProductID"] == product_id), None)
 
         if not product:
-            return "Product not found", 404
+            return render_template("404.html"), 500
         else:
-            product["Image 1"] = convert_link(product["Image 1"])
-            product["Image 2"] = convert_link(product["Image 2"])
-            product["Image 3"] = convert_link(product["Image 3"])
-            product["Image 4"] = convert_link(product["Image 4"])
-            product["Image 5"] = convert_link(product["Image 5"])
-            product["Image 6"] = convert_link(product["Image 6"])
-        
+            for i in range(1, 7):
+                key = f"Image {i}"
+                if key in product:
+                    product[key] = convert_link(product[key])
+
         return render_template("product.html", all_categories=categories, product=product)
     except gspread.exceptions.WorksheetNotFound:
         return render_template("404.html"), 500
