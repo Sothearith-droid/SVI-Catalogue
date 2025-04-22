f371145f35f53f2566fcdc40aeb9af2b8dbef2d0
Sothearith Tith <captainteemo32@gmail.com>
2025-04-22 13:51:05 +1000

    updated 404 page to be responsive

--- static/script.js
+++ static/script.js
@@ -1,21 +1,21 @@
 // Category swiper
 document.addEventListener("DOMContentLoaded", function () {
-const swiper = new Swiper(".homeSwiper", {
-    loop: true,
-    speed: 1000,
-    autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-    },
-    pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    },
-    navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-    },
-});
+    const swiper = new Swiper(".homeSwiper", {
+        loop: true,
+        speed: 1000,
+        autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+        },
+        pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        },
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        },
+    });
 });
 
 
@@ -40,31 +40,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 document.addEventListener("DOMContentLoaded", () => {
     const loader = document.getElementById("loader");
-  
+
     const hideLoader = () => {
-      if (loader) {
+        if (loader) {
         loader.classList.add("hidden");
-      }
+        }
     };
-  
+
     // 1. Hide loader when the page fully loads
     window.addEventListener("load", () => {
-      setTimeout(hideLoader, 300);
+        setTimeout(hideLoader, 300);
     });
-  
+
     // 2. Hide loader if page was loaded from back/forward cache
     window.addEventListener("pageshow", (event) => {
-      if (event.persisted || performance.getEntriesByType("navigation")[0]?.type === "back_forward") {
+        if (event.persisted || performance.getEntriesByType("navigation")[0]?.type === "back_forward") {
         hideLoader();
-      }
+        }
     });
-  
+
     // 3. Show loader on link clicks for page navigation
     document.querySelectorAll("a").forEach(link => {
-      link.addEventListener("click", e => {
+        link.addEventListener("click", e => {
             const href = link.getAttribute("href");
             const target = link.getAttribute("target");
-    
+
             if (
                 href &&
                 !href.startsWith("#") &&
